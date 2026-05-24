95b5664fac5840f14087afb008b71a7454efb849
yashsharma044 <yashjnvambala@gmail.com>
2026-05-24 08:33:11 +0530

    Reset Project and add Navigation

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,9 +1,9 @@
 import { Stack } from 'expo-router'
 
-export default function RootLayout() {
+export default function HomeLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
     </Stack>
   )
 }
---
+++ app/(tabs)/home/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import HomeScreen from "@/src/screens/HomeScreen";
+
+const index = () => {
+  return <HomeScreen />;
+};
+
+export default index;
